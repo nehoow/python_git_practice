dba1f6f04e69524623925a4c935c8731d6304e80
Nyks <redgo3666@gmail.com>
2025-03-11 20:14:16 +0800

    Finished display_lowest_number.py

---
+++ python_practice3/display_lowest_number.py
@@ -0,0 +1,17 @@
+#Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+numbers = []
+lesser = None  
+
+while True:
+    try:
+        user_input = int(input("Input a number: "))
+        numbers.append(user_input)
+
+        if lesser is None or user_input < lesser:
+            lesser = user_input
+
+        print("Lowest so far:", lesser)
+
+    except ValueError:
+        print("\nFinal lowest number:", lesser)
+        break 
