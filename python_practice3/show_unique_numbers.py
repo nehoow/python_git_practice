ec7f1a1e52f35375204b43ee7284b068b29c6c78
Nyks <redgo3666@gmail.com>
2025-03-11 19:12:15 +0800

    Finished show_unique_numbers.py, displaying unique numbers

---
+++ python_practice3/show_unique_numbers.py
@@ -0,0 +1,11 @@
+#Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+#loop input 10
+#loop check /= elements list print
+numbers = []
+for i in range(10):
+    user_input = int(input("Input numbers: "))
+    numbers.append(user_input)
+
+for num in numbers:
+    if numbers.count(num) == 1:
+        print(num)
