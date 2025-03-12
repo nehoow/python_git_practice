00468db41fcec1ecb2a44a108447e5b38a72843d
Nyks <redgo3666@gmail.com>
2025-03-12 14:17:11 +0800

    Finished print_difference_ten_numbers.py

--- python_practice2/print_difference_ten_numbers.py
+++ python_practice2/print_difference_ten_numbers.py
@@ -1,1 +1,7 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+user_input = int(input("Input a number:"))
+difference = user_input
+for i in range(9):
+    user_input = int(input("Input a number:"))
+    difference -= user_input
+print("Result:", difference)
