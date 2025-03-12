d26fc725115fcf3b67f2ab0229d50c5453448e94
Nyks <redgo3666@gmail.com>
2025-03-12 15:07:40 +0800

    Finished print_quotient_integer.py

--- python_practice2/print_quotient_integer.py
+++ python_practice2/print_quotient_integer.py
@@ -1,1 +1,5 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+quotient = first_user_input / second_user_input
+print (quotient)
