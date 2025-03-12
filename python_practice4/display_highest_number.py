ddcefe5fb90abd9ad25dd535ab557eb374420d79
Nyks <redgo3666@gmail.com>
2025-03-12 13:17:39 +0800

    Finished and tested display_highest_number.py!

--- python_practice4/display_highest_number.py
+++ python_practice4/display_highest_number.py
@@ -1,1 +1,10 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+highest_number = None
+while True:
+    try:
+        user_input = int(input("Input a number: "))
+        if highest_number is None or user_input > highest_number:
+            highest_number = user_input
+    except ValueError:
+        print ("Highest Number:", highest_number)
+        break
