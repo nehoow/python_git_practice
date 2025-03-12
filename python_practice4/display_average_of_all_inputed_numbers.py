7a7a0ee820b4965114e6038ff2ac25712254a570
Nyks <redgo3666@gmail.com>
2025-03-12 13:41:01 +0800

    Finished and tested display_average_of_all_inputed_numbers.py

--- python_practice4/display_average_of_all_inputed_numbers.py
+++ python_practice4/display_average_of_all_inputed_numbers.py
@@ -1,1 +1,13 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+sum = 0
+count = 0
+while True:
+    try:
+        user_input = float(input("Input a number: "))
+        sum += user_input
+        count += 1
+
+    except ValueError:
+        print ("Invalid Input.")
+        print ("Average:",sum / count)
+        break
