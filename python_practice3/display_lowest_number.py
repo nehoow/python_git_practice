722674c2ffe9d90c2a0438289d110a681986dffd
Nyks <redgo3666@gmail.com>
2025-03-12 10:57:24 +0800

    Removed numbers list at display_lowest_number.py as it was unnecessary

--- python_practice3/display_lowest_number.py
+++ python_practice3/display_lowest_number.py
@@ -1,11 +1,9 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
-numbers = []
 lesser = None  
 
 while True:
     try:
         user_input = int(input("Input a number: "))
-        numbers.append(user_input)
 
         if lesser is None or user_input < lesser:
             lesser = user_input
