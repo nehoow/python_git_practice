ca645928986e4c51f98e8285da5faad9b31b20b5
Nyks <redgo3666@gmail.com>
2025-03-12 14:10:42 +0800

    Finished print_sum_two_numbers.py

--- python_practice1/print_sum_two_numbers.py
+++ python_practice1/print_sum_two_numbers.py
@@ -1,1 +1,5 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+sum = first_user_input + second_user_input
+print (sum)
