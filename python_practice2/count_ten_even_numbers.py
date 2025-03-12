4dfbbd31bbd579cc2d035c128deaa9280ed0ffe1
Nyks <redgo3666@gmail.com>
2025-03-12 14:12:29 +0800

    Finished count_ten_even_numbers.py

--- python_practice2/count_ten_even_numbers.py
+++ python_practice2/count_ten_even_numbers.py
@@ -1,1 +1,7 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+count = 0
+for i in range(10):
+    user_input = int(input("Input a number: "))
+    if user_input % 2 == 0:
+        count += 1
+print ("Even Numbers count:", count)
