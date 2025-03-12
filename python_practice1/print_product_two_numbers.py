48b2e46f7d1fa98030e9c6e3156cbe1b3f3fdd63
Nyks <redgo3666@gmail.com>
2025-03-12 14:03:38 +0800

    Finished print_product_two_numbers.py

--- python_practice1/print_product_two_numbers.py
+++ python_practice1/print_product_two_numbers.py
@@ -1,1 +1,5 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+product = first_user_input * second_user_input
+print (product)
