f72a28f54e7409f04fd725f70574a647581f5042
Nyks <redgo3666@gmail.com>
2025-03-12 14:59:01 +0800

    Finished print_difference.py

--- python_practice2/print_difference.py
+++ python_practice2/print_difference.py
@@ -1,1 +1,5 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+difference = first_user_input - second_user_input
+print (difference)
