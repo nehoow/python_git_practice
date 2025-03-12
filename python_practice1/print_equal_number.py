3e8a10870dec9da80728f71958d6f6c852735f07
Nyks <redgo3666@gmail.com>
2025-03-12 13:54:23 +0800

    Finished print_equal_number.py

--- python_practice1/print_equal_number.py
+++ python_practice1/print_equal_number.py
@@ -1,1 +1,5 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+if first_user_input == second_user_input:
+    print("They are equal.")
