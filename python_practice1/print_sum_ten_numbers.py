783d8ad997bd47912ee567407b9379f87a07e6e9
Nyks <redgo3666@gmail.com>
2025-03-12 14:09:49 +0800

    Fixed a mistake and changed all the code of print_sum_ten_numbers.py

--- python_practice1/print_sum_ten_numbers.py
+++ python_practice1/print_sum_ten_numbers.py
@@ -1,7 +1,6 @@
-#Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
-count = 0
+#Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+sum = 0
 for i in range(10):
     user_input = int(input("Input a number: "))
-    if user_input % 2 == 0:
-        count += 1
-print ("Even numbers:", count)
+    sum += user_input
+print("Sum of all numbers:", sum)
