60de278298723bec90dae3332bc322cb4b3d1300
Nyks <redgo3666@gmail.com>
2025-03-12 14:14:39 +0800

    Finished print_between_two_numbers.py

--- python_practice2/print_between_two_numbers.py
+++ python_practice2/print_between_two_numbers.py
@@ -1,1 +1,11 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+if first_user_input == second_user_input:
+    print("They are equal.")
+elif first_user_input > second_user_input:
+    for i in range(second_user_input + 1, first_user_input):
+        print (i)
+else:
+     for i in range(first_user_input + 1, second_user_input):
+        print (i)
