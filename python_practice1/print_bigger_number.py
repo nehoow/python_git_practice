99b58505e566ba867a25447995efc645bef39c41
Nyks <redgo3666@gmail.com>
2025-03-12 13:53:45 +0800

    Fixed print_bigger_number.py printing bigger number even they are equal.

--- python_practice1/print_bigger_number.py
+++ python_practice1/print_bigger_number.py
@@ -3,7 +3,7 @@ first_user_input = int(input("First Number: "))
 second_user_input = int(input("Second Number: "))
 if first_user_input == second_user_input:
     print("They are equal.")
-if first_user_input > second_user_input:
-    print("Bigger Number", first_user_input)
+elif first_user_input > second_user_input:
+    print("Bigger Number:", first_user_input)
 else:
-    print("Bigger Number", second_user_input)
+    print("Bigger Number:", second_user_input)
