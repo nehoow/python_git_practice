2819717391d1cfa3a18158c4e24cb4dbe90f8263
Nyks <redgo3666@gmail.com>
2025-03-12 13:08:25 +0800

    Finished and tested display_most_repeating_number.py

--- python_practice4/display_most_repeating_number.py
+++ python_practice4/display_most_repeating_number.py
@@ -1,1 +1,18 @@
 #Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+numbers = []
+max_count = 0
+most_repeating = None
+while True:
+    try:
+        user_input = int(input("Input a number: "))
+        numbers.append(user_input)
+
+        for num in set(numbers):
+            count = numbers.count(num)
+            if count > max_count:
+                max_count = count
+                most_repeating = num
+    except ValueError:
+        print("Invalid Input.")
+        print("Most repeated number:", most_repeating)
+        break
