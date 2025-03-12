eb29695c127c1bfd75a0a225c27109522a9b06be
Nyks <redgo3666@gmail.com>
2025-03-12 15:09:57 +0800

    Finished print_smaller_number.py

--- python_practice2/print_smaller_number.py
+++ python_practice2/print_smaller_number.py
@@ -1,1 +1,9 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+if first_user_input == second_user_input:
+    print("They are equal.")
+elif first_user_input < second_user_input:
+    print("Smaller Number:", first_user_input)
+else:
+    print("Smaller Number:", second_user_input)
