df4a83043dc036f9b875918912e7aaf91463539f
Nyks <redgo3666@gmail.com>
2025-03-12 14:04:30 +0800

    Finished print_quotient_float.py

--- python_practice1/print_quotient_float.py
+++ python_practice1/print_quotient_float.py
@@ -1,1 +1,5 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+first_user_input = float(input("First Number: "))
+second_user_input = float(input("Second Number: "))
+quotient = first_user_input / second_user_input
+print (quotient)
