cdaef099790b981ed5eef900c72c767e9fe276af
Nyks <redgo3666@gmail.com>
2025-03-12 14:59:38 +0800

    Finished print_not_equal_number.py

--- python_practice2/print_not_equal_number.py
+++ python_practice2/print_not_equal_number.py
@@ -1,1 +1,5 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+if first_user_input != second_user_input:
+    print("They are not equal.")
