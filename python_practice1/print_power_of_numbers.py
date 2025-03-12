e54d75df71312d8c06a6d6b91b5a0fc7a00614ab
Nyks <redgo3666@gmail.com>
2025-03-12 14:02:55 +0800

    Finished print_power_of_numbers.py

--- python_practice1/print_power_of_numbers.py
+++ python_practice1/print_power_of_numbers.py
@@ -1,1 +1,5 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+power = first_user_input ** second_user_input
+print (power)
