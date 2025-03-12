53f04313bd92a51105e08e47fdaeae0ba85dc70e
Nyks <redgo3666@gmail.com>
2025-03-12 13:50:42 +0800

    Finished print_bigger_number.py

--- python_practice1/print_bigger_number.py
+++ python_practice1/print_bigger_number.py
@@ -1,1 +1,9 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+if first_user_input == second_user_input:
+    print("They are equal.")
+if first_user_input > second_user_input:
+    print("Bigger Number", first_user_input)
+else:
+    print("Bigger Number", second_user_input)
