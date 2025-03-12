d0f77ffae99cce3fa2f7d9c170712d6ed44d6700
Nyks <redgo3666@gmail.com>
2025-03-12 15:08:47 +0800

    Finished print_remainder.py

--- python_practice2/print_remainder.py
+++ python_practice2/print_remainder.py
@@ -1,1 +1,5 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+first_user_input = int(input("First Number: "))
+second_user_input = int(input("Second Number: "))
+remainder = first_user_input % second_user_input
+print (remainder)
