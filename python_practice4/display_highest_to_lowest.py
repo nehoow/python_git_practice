95593ecc8ad8eb7d2e91e858ab9198ba7d20cf5e
Nyks <redgo3666@gmail.com>
2025-03-12 13:21:44 +0800

    Finished and tested display_highest_to_lowest.py

--- python_practice4/display_highest_to_lowest.py
+++ python_practice4/display_highest_to_lowest.py
@@ -1,1 +1,11 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+numbers = []
+while True:
+    try:
+        user_input = int(input("Input a number: "))
+        numbers.append(user_input)
+    except ValueError:
+        print("Invalid Input")
+        numbers.sort(reverse=True)
+        print("Numbers from highest to lowest:", numbers)
+        break
