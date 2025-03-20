ec715b83ccc468068375f447064b6bcd97f57f81
Nyks <redgo3666@gmail.com>
2025-03-20 13:35:36 +0800

    Finished program 5 batch 5.

--- python_practice5/proper_casing_name.py
+++ python_practice5/proper_casing_name.py
@@ -4,5 +4,8 @@
 #Output: Juan Dela Cruz
 
 #ask the user to input their fullanme in incorrect casing
+full_name = input("enter your fullname with incorrect casing: ")
 #Use .title() to fix and put proper casing to the name
+proper_casing_name = full_name.title()
 #print the proper cased name
+print(proper_casing_name)
