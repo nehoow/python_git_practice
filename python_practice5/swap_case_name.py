669eea914135629c0c5507e68f2eda700c391f9e
Nyks <redgo3666@gmail.com>
2025-03-20 13:39:08 +0800

    Finished program 6 batch 5.

--- python_practice5/swap_case_name.py
+++ python_practice5/swap_case_name.py
@@ -4,5 +4,8 @@
 #Output: JuaN deLA cRuz
 
 #ask user to input their fullname with incorrect casing
+full_name = input("Enter your fullname with incorrect casing: ")
 #reverse the casing by using .swapcase()
+swapped_case_name = full_name.swapcase()
 #print sa swapped cased name
+print(swapped_case_name)
