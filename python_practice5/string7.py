0730fb7fccd562aa866753bedd8e72e5c30b0268
Nyks <redgo3666@gmail.com>
2025-03-20 13:46:13 +0800

    Added pseudocode program 7 batch 5.

---
+++ python_practice5/string7.py
@@ -0,0 +1,9 @@
+#Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+#Example:
+#Input: We will weather the weather whatever the weather whether we like it or not
+#Output: 14
+
+#Ask the user to input a complete statement
+#split the string to different words using .split
+#we will count the number of words inside the list of the split
+#print count
