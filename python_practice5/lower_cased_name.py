64fda989d9f68c9fafec7e37331c29e1e5d7f0c8
Nyks <redgo3666@gmail.com>
2025-03-20 13:31:03 +0800

    Added pseudocode to program 4 batch 5, renamed the file to be more descriptive.

---
+++ python_practice5/lower_cased_name.py
@@ -0,0 +1,8 @@
+#Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+#Example:
+#Input: Juan Dela Cruz
+#Output: juan dela cruz
+
+#Ask the user input for their full name
+#lower case everything by using .lower()
+#print the lower cased name
