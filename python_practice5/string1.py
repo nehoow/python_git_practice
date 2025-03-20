7e4e6ca5248fea993636144ecea1360368ee3e18
Nyks <redgo3666@gmail.com>
2025-03-20 13:02:08 +0800

    Rough pseudocode of batch 5 problem 1

---
+++ python_practice5/string1.py
@@ -0,0 +1,7 @@
+#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+#Example:
+#Input:         Juan Dela Cruz
+#Output: Juan Dela Cruz
+#ask the user input()
+#l.strip to remove the spaces in the beginning
+#print result
