66ac9e5440d5591a4b1d0aa43dda781584dd6517
Nyks <redgo3666@gmail.com>
2025-03-21 19:28:36 +0800

    Finished and renamed  pseudocode program 7 batch 5.

--- python_practice5/word_count.py
+++ python_practice5/word_count.py
@@ -4,6 +4,10 @@
 #Output: 14
 
 #Ask the user to input a complete statement
+statement = input("Input a whole statement: ")
 #split the string to different words using .split
+word = statement.split()
 #we will count the number of words inside the list of the split
+word_count = len(word)
 #print count
+print(word_count)
