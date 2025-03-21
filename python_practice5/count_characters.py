d63e872a1b8f8b10ddd4fdb5c8060c75dd1b668f
Nyks <redgo3666@gmail.com>
2025-03-21 19:32:17 +0800

    Added pseudocode program 8 batch 5.

---
+++ python_practice5/count_characters.py
@@ -0,0 +1,8 @@
+#Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+#Example:
+#Input: Juan Dela Cruz
+#Output: 14
+
+#ask the user for their full name
+#count the characters using len()
+#print the result
