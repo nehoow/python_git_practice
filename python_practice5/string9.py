6a83cadb5475fe4abeed2251fb5d2fb44c1aa651
Nyks <redgo3666@gmail.com>
2025-03-21 20:04:39 +0800

    Added a pseudocode to program 9 batch 5

---
+++ python_practice5/string9.py
@@ -0,0 +1,8 @@
+#Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: JuanDelaCruz
+
+#ask the user their full name in incorrect casing
+#capitalize everything and remove space
+#print result
