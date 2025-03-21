c92a302a747fc14b37b74ac696d4f765c26b894c
Nyks <redgo3666@gmail.com>
2025-03-21 20:07:09 +0800

    Added a pseudocode to program 10 batch 5

---
+++ python_practice5/string10.py
@@ -0,0 +1,8 @@
+#Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: juan_dela_cruz
+
+#ask the user in incorrect casing
+#lower case everything with lower and replace all space to _
+#print the result
