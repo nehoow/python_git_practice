e0754b506864d685e9a8e17d4400505164f41791
Nyks <redgo3666@gmail.com>
2025-03-21 20:06:07 +0800

    Finished program 9 batch 5 and renamed the file

--- python_practice5/pascal_case_name.py
+++ python_practice5/pascal_case_name.py
@@ -4,5 +4,8 @@
 #Output: JuanDelaCruz
 
 #ask the user their full name in incorrect casing
+name = input("Enter your fullname in incorrect casing: ")
 #capitalize everything and remove space
+pascal_case_name = name.title().replace(' ','')
 #print result
+print(pascal_case_name)
