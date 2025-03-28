000289228fce225a496d5046ab5f081501ca41f1
Nyks <redgo3666@gmail.com>
2025-03-28 22:22:29 +0800

    Finished and renamed program 6.

--- python_practice6/6.py
+++
@@ -1,2 +0,0 @@
-#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
-# i would print the filler characters until desired length of the difference of desiredlength and the string.
